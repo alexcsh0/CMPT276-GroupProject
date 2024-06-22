7506738fa878942124de547d056a3deb3c0d5822
Daleniant <vladusinskm@gmail.com>
2024-06-21 21:18:35 -0700

    Setting up basic react into project

---
+++ frontend/src/components/pages/error/error.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useRouteError } from 'react-router-dom';
+
+interface Error {
+  statusText: string | null
+  message: string
+}
+
+/**
+ * Generic Error Page
+ * @returns error page element
+ */
+export function ErrorPage() {
+  const error = useRouteError() as Error;
+
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error.statusText ?? error.message}</i>
+      </p>
+    </div>
+  );
+}
---
+++ frontend/src/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { ErrorPage } from './components/pages/error/error';
+import { Dashboard } from './components/pages/dashboard/dahsboard';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+// Define the routes for the application and components to display
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Dashboard />,
+    errorElement: <ErrorPage />
+  }
+]);
+
+// Render the application with routes
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
