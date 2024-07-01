02018903c43a3230b4d01495177ba115747b69bc
Alex Chung <alexcssh27@gmail.com>
2024-07-01 12:26:25 -0700

    center pages in navbar

--- frontend/src/components/common/pages/navbar.tsx
+++ frontend/src/components/common/pages/navbar.tsx
@@ -51,7 +51,7 @@ const Navbar: React.FC<Props> = ({ role }) => {
     }
 
     return (
-      <Box sx={{ display: { xs: 'none', md: 'flex' }, ml: 2 }}>
+      <Box sx={{ display: { xs: 'none', md: 'flex' }, justifyContent: 'center', flexGrow: 1 }}>
         {pages.map((page) => (
           <Button
             key={page}
@@ -69,83 +69,86 @@ const Navbar: React.FC<Props> = ({ role }) => {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="fixed">
         <Toolbar>
-          <DirectionsTransitIcon className={styles.logo} />
-          <Typography variant="h6" component="div" sx={{ display: 'flex', alignItems: 'center' }}>
-            Transit App
-            {renderNavItems()}
-          </Typography>
-          <Box sx={{ flexGrow: 1 }} />
-          {role === 'newUser' ? (
-            <>
-              <Button color="inherit">Login</Button>
-              <Button color="inherit">Sign up</Button>
-            </>
-          ) : (
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-              {role === 'existingUser' || role === 'admin' ? (
-                <>
-                  <IconButton
-                    size="large"
-                    aria-label="show 17 new notifications"
-                    color="inherit"
-                    sx={{ mr: 2 }}
-                    onClick={handleOpenNotificationsMenu}
-                  >
-                    <Badge badgeContent={17} color="error">
-                      <NotificationsIcon />
-                    </Badge>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <DirectionsTransitIcon className={styles.logo} />
+            <Typography variant="h6" component="div">
+              Transit App
+            </Typography>
+          </Box>
+          {renderNavItems()}
+          <Box sx={{ display: 'flex', alignItems: 'center', ml: 'auto' }}>
+            {role === 'newUser' ? (
+              <>
+                <Button color="inherit">Login</Button>
+                <Button color="inherit">Sign up</Button>
+              </>
+            ) : (
+              <>
+                {role === 'existingUser' || role === 'admin' ? (
+                  <>
+                    <IconButton
+                      size="large"
+                      aria-label="show 17 new notifications"
+                      color="inherit"
+                      sx={{ mr: 2 }}
+                      onClick={handleOpenNotificationsMenu}
+                    >
+                      <Badge badgeContent={17} color="error">
+                        <NotificationsIcon />
+                      </Badge>
+                    </IconButton>
+                    <Menu
+                      sx={{ mt: '45px' }}
+                      id="menu-notifications"
+                      anchorEl={anchorElNotifications}
+                      anchorOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                      }}
+                      keepMounted
+                      transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'right',
+                      }}
+                      open={Boolean(anchorElNotifications)}
+                      onClose={handleCloseNotificationsMenu}
+                    >
+                      <MenuItem onClick={handleCloseNotificationsMenu}>Alert 1</MenuItem>
+                      <MenuItem onClick={handleCloseNotificationsMenu}>Alert 2</MenuItem>
+                      <MenuItem onClick={handleCloseNotificationsMenu}>Alert 3</MenuItem>
+                    </Menu>
+                  </>
+                ) : null}
+                <Tooltip title="Open settings">
+                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar alt="User" src="/static/images/avatar/2.jpg" />
                   </IconButton>
-                  <Menu
-                    sx={{ mt: '45px' }}
-                    id="menu-notifications"
-                    anchorEl={anchorElNotifications}
-                    anchorOrigin={{
-                      vertical: 'top',
-                      horizontal: 'right',
-                    }}
-                    keepMounted
-                    transformOrigin={{
-                      vertical: 'top',
-                      horizontal: 'right',
-                    }}
-                    open={Boolean(anchorElNotifications)}
-                    onClose={handleCloseNotificationsMenu}
-                  >
-                    <MenuItem onClick={handleCloseNotificationsMenu}>Alert 1</MenuItem>
-                    <MenuItem onClick={handleCloseNotificationsMenu}>Alert 2</MenuItem>
-                    <MenuItem onClick={handleCloseNotificationsMenu}>Alert 3</MenuItem>
-                  </Menu>
-                </>
-              ) : null}
-              <Tooltip title="Open settings">
-                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                  <Avatar alt="User" src="/static/images/avatar/2.jpg" />
-                </IconButton>
-              </Tooltip>
-              <Menu
-                sx={{ mt: '45px' }}
-                id="menu-appbar"
-                anchorEl={anchorElUser}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={Boolean(anchorElUser)}
-                onClose={handleCloseUserMenu}
-              >
-                {settings.map((setting) => (
-                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                    <Typography textAlign="center">{setting}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
-            </Box>
-          )}
+                </Tooltip>
+                <Menu
+                  sx={{ mt: '45px' }}
+                  id="menu-appbar"
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={Boolean(anchorElUser)}
+                  onClose={handleCloseUserMenu}
+                >
+                  {settings.map((setting) => (
+                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                      <Typography textAlign="center">{setting}</Typography>
+                    </MenuItem>
+                  ))}
+                </Menu>
+              </>
+            )}
+          </Box>
         </Toolbar>
       </AppBar>
     </Box>
