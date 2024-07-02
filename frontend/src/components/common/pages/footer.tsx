5d4cd66f9ea420242efbf702016a0412e9bb4103
Alex Chung <alexcssh27@gmail.com>
2024-07-02 15:41:14 -0700

    small duplicate fix

--- frontend/src/components/common/pages/footer.tsx
+++ frontend/src/components/common/pages/footer.tsx
@@ -6,8 +6,7 @@ export function Footer() {
     return (
         <Box className={styles.footer}>
             <Typography variant="body1" className={styles.text}>
-                Created by: Harold Chang, Tingyu Chen, Taewoong Choi, Alex Chung
-                Alex Chung, Vladislav Mironov
+                Created by: Harold Chang, Tingyu Chen, Taewoong Choi, Alex Chung, Vladislav Mironov
             </Typography>
             <Typography variant="body2" className={styles.text}>
                 &copy; {new Date().getFullYear()} Route Alert. All rights reserved.
