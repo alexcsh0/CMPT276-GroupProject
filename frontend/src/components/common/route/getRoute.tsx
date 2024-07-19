0c59530eb665eaf90c9eb02aeb529d90f4ece656
Harold Chang <haroldchang@hotmail.com>
2024-07-19 07:20:23 -0700

    modified route page

--- frontend/src/components/common/route/getRoute.tsx
+++ frontend/src/components/common/route/getRoute.tsx
@@ -1,4 +1,5 @@
-import { useState, useMemo, useRef, useEffect } from 'react';
+import { useState, useMemo, useRef } from 'react';
+import { Switch } from "@mui/material";
 import {
     Box, 
     Button,
@@ -15,7 +16,9 @@ import {
     Autocomplete,
 } from '@react-google-maps/api';
 import { VscChromeClose } from 'react-icons/vsc';
-import { FaHome } from "react-icons/fa";
+import { 
+    FaHome,
+} from "react-icons/fa";
 
 
 type latLngLiteral = google.maps.LatLngLiteral;
@@ -29,10 +32,11 @@ export function GetRoute() {
     const [directionsResponse, setDirectionsResponse] = useState<DirectionsResult | null>(null);
     const [distance, setDistance] = useState('');
     const [duration, setDuration] = useState('');
-    const [amountOfRoutes, setRouteAmount] = useState('');
     const originRef = useRef<HTMLInputElement>(null);
     const destinationRef = useRef<HTMLInputElement>(null);
-    
+    const [checked, setChecked] = useState(false);
+    const [shown, isShown] = useState(false);
+
     async function calculateRoute() {
         if (originRef.current!.value === '' || destinationRef.current!.value === ' ') {
             return;
@@ -44,27 +48,99 @@ export function GetRoute() {
             travelMode: google.maps.TravelMode.TRANSIT,
             provideRouteAlternatives: true,
         });
+
         setDirectionsResponse(results);
-        
-        console.log(results.routes);
         setDistance(results.routes[0].legs[0].distance!.text);
         setDuration(results.routes[0].legs[0].duration!.text);
-        setRouteAmount(results.routes.length.toString());
+        displayFirstRoute(results);
+    }
+
+    function displayFirstRoute(directionsResponse: google.maps.DirectionsResult) {
+
+        if (!shown) {
+            isShown(!shown);
+            let parentNode = document.getElementById("firstRoute");
+            let instructions = document.createElement("ol");
+            let routeHeader = document.createElement("h3");
+            routeHeader.innerHTML = "From " + directionsResponse?.routes[0].legs[0].departure_time?.text + " to " + directionsResponse?.routes[0].legs[0].arrival_time?.text + " ( " + directionsResponse?.routes[0].legs[0].duration?.text + " )";
+            parentNode?.appendChild(routeHeader)
+            let numberOfSteps = Number(directionsResponse?.routes[0].legs[0].steps.length);
+            for (let i = 0; i < numberOfSteps; i++) {
+                let step = document.createElement("li");
+                step.innerHTML = (directionsResponse?.routes[0].legs[0].steps[i].instructions)!;
+                instructions?.appendChild(step);
+            }
+            parentNode?.appendChild(instructions);
+        }
     }
 
     function displayRoute() {
 
-        // work in progress
-        for (let i = 0; i < Number(amountOfRoutes); i++) {
-            console.log("hello");
+        setChecked(!checked);
+
+        if (!checked) {
+            let amountOfAltRoutes = Number(directionsResponse?.routes.length); // returns the number of possible routes (through transit only) available from point a to b
+    
+            for (let i = 1; i < amountOfAltRoutes; i++) {
+                if (validateRoute(i)) {
+    
+                    let node = document.getElementById("transitRoutes");
+                    let altRoute = document.createElement("li");
+                    let instructions = document.createElement("ol");
+                    let routeHeader = document.createElement("h2");
+                    instructions.className = "instructions";
+                    altRoute.className = "routes";
+                    routeHeader.innerHTML = "From " + directionsResponse?.routes[i].legs[0].departure_time?.text + " to " + directionsResponse?.routes[i].legs[0].arrival_time?.text + " ( " + directionsResponse?.routes[i].legs[0].duration?.text + " )";
+
+                    let numberOfSteps = Number(directionsResponse?.routes[i].legs[0].steps.length);
+                    for (let j = 0; j < numberOfSteps; j++) {
+                        let step = document.createElement("li");
+                        step.innerHTML = (directionsResponse?.routes[i].legs[0].steps[j].instructions)!;
+                        instructions.appendChild(step);
+                    }
+        
+                    altRoute.appendChild(routeHeader);
+                    altRoute.appendChild(instructions);
+                    node?.appendChild(altRoute);
+                }
+            }
+        }
+        else {
+            let altRoutes = document.getElementById("transitRoutes");
+            altRoutes!.innerHTML = "";
         }
     }
 
+    function validateRoute(i: number) {
+       
+        for (let j = i - 1; j >= 0; j--) {
+            
+            let currentRouteStepCount = Number(directionsResponse?.routes[i].legs[0].steps.length)
+            let comparedRouteStepCount = Number(directionsResponse?.routes[j].legs[0].steps.length)
+            let stepCount = currentRouteStepCount;
+            if ( currentRouteStepCount < comparedRouteStepCount) {
+                stepCount = comparedRouteStepCount;
+            }
+
+            for (let k = 0; k < stepCount; k++) {
+
+                if (directionsResponse?.routes[i].legs[0].steps[k].instructions != directionsResponse?.routes[j].legs[0].steps[k].instructions) { 
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
     function clearRoute() {
+        isShown(!shown);
+        let instructions = document.getElementById("firstRoute");
+        instructions!.innerHTML = "";
+        let altRoutes = document.getElementById("transitRoutes");
+        altRoutes!.innerHTML = "";
         setDirectionsResponse(null);
         setDistance('');
         setDuration('');
-        setRouteAmount('');
         originRef.current!.value = '';
         destinationRef.current!.value = '';
     }
@@ -73,24 +149,37 @@ export function GetRoute() {
         {/* Search Bar (Displayed on the left) */}
         <div className={Styles.searchBar}>
             <h1>Search</h1>
-            <HStack spacing={2} justifyContent='space-between'>
-                <Box flexGrow={1}>
-                    <Autocomplete>
-                        <Input type='text' placeholder='Origin' ref={originRef}/>
-                    </Autocomplete>
-                </Box>
-                <Box flexGrow={1}>
-                    <Autocomplete>
-                        <Input type='text' placeholder='Destination' ref={destinationRef}/>
-                    </Autocomplete>
-                </Box>
+            <HStack spacing={2} justifyContent='space-between' alignItems="initial">
+                
+                <div className={Styles.search}>
+                    <Box flexGrow={1}>
+                        <Autocomplete>
+                            <Input type='text' placeholder='Origin' ref={originRef}/>
+                        </Autocomplete>
+                    </Box>
+                    <Box flexGrow={1}>
+                        <Autocomplete>
+                            <Input type='text' placeholder='Destination' ref={destinationRef}/>
+                        </Autocomplete>
+                    </Box>
 
-                <ButtonGroup>
-                    <Button type='submit'onClick={calculateRoute}>
-                        Calculate Route
-                    </Button>
-                    <IconButton aria-label='center back' icon={<VscChromeClose />} onClick={clearRoute}/>
-                </ButtonGroup>
+                    <ButtonGroup className={Styles.buttonGroup}>
+                        <Button type='submit'onClick={calculateRoute}>
+                            Calculate Route
+                        </Button>
+                        <IconButton aria-label='center back' icon={<VscChromeClose />} onClick={clearRoute}/>
+                        <IconButton aria-label='center back' icon={<FaHome />} onClick={() => {
+                            map!.panTo(startingPosition)
+                            map!.setZoom(17);
+                        }}/>
+                    </ButtonGroup>
+                </div>
+                <aside className={Styles.steps}>
+                    <h2>Instructions</h2>
+                    <div id="firstRoute">
+
+                    </div>
+                </aside>
             </HStack>
 
             <HStack spacing={2} justifyContent='space-between'>
@@ -98,21 +187,21 @@ export function GetRoute() {
                     <Text>Distance: {distance}</Text>
                     <Text>Duration: {duration}</Text>
                 </Box>
-                <IconButton aria-label='center back' icon={<FaHome />} onClick={() => {
-                    map!.panTo(startingPosition)
-                    map!.setZoom(17);
-                }}/>
             </HStack>
-            <h2>Alternative Routes: {amountOfRoutes}</h2>
-            <ul>
-                {/* work in progress */}
-            </ul>
+            {/* <IconButton aria-label='center back' icon={<FaRoute fontSize={"large"} />} onClick={displayRoute}/> */}
+            <h2>Show Alternative Routes :  
+            
+            <Switch checked={checked} onChange={displayRoute}/>
+            
+            </h2>
+            <ol id="transitRoutes">
+            </ol>
         </div>
 
         {/* Map (Displayed on the right) */}
         <div className={Styles.map}>
             <GoogleMap zoom={17} center={startingPosition} mapContainerClassName={Styles.mapContainer} onLoad={(map) => setMap(map)}>
-
+                
                 {directionsResponse && <DirectionsRenderer directions={directionsResponse}/>}
             </GoogleMap>
         </div>
